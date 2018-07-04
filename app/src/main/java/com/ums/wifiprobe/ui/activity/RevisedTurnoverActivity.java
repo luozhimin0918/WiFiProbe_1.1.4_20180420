ce16786ce7edf079601243f5b7b0d11063556fa4
luozhimin <838111691@qq.com>
2018-07-04 17:23:29 +0800

    修正交易额Activity  返回事件和弹框提示修正交易额

--- app/src/main/java/com/ums/wifiprobe/ui/activity/RevisedTurnoverActivity.java
+++ app/src/main/java/com/ums/wifiprobe/ui/activity/RevisedTurnoverActivity.java
@@ -1,16 +1,22 @@
 package com.ums.wifiprobe.ui.activity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ums.wifiprobe.R;
+import com.ums.wifiprobe.ui.customview.OnDialogCloseListener;
 import com.ums.wifiprobe.utils.DeviceUtil;
+import com.ums.wifiprobe.utils.DialogUtil;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 
 /**
@@ -22,6 +28,18 @@ public class RevisedTurnoverActivity extends BaseActivity implements View.OnClic
 
     @BindView(R.id.status_bar)
     LinearLayout statusBar;
+    @BindView(R.id.ChangSubmit)
+    TextView ChangSubmit;
+    @BindView(R.id.edit_query)
+    EditText editQuery;
+    @BindView(R.id.changEn)
+    TextView changEn;
+    @BindView(R.id.edit_query_bilie)
+    EditText editQueryBilie;
+    @BindView(R.id.edit_query_mianji)
+    EditText editQueryMianji;
+    @BindView(R.id.head_back)
+    LinearLayout headBack;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,7 +54,7 @@ public class RevisedTurnoverActivity extends BaseActivity implements View.OnClic
     @Override
     public void initView() {
         //设置状态栏高度
-        int  statusBarHeight =DeviceUtil.getStatusHeight(this);
+        int statusBarHeight = DeviceUtil.getStatusHeight(this);
         LinearLayout.LayoutParams sp_params = new LinearLayout.LayoutParams(
                 RelativeLayout.LayoutParams.MATCH_PARENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
@@ -71,4 +89,23 @@ public class RevisedTurnoverActivity extends BaseActivity implements View.OnClic
 
     }
 
+    @OnClick({R.id.ChangSubmit,R.id.head_back})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ChangSubmit:
+                //提醒从应用市场下载安装
+                DialogUtil.showCommonDialog2(this, "   亲爱的老板，您上次预估的修正幅度为30%，确定要将修正幅度改为-30%？", new OnDialogCloseListener() {
+                    @Override
+                    public void onClick(Dialog dialog, boolean confirm) {
+                        if (confirm) {
+
+                        }
+                    }
+                });
+                break;
+            case R.id.head_back:
+                finish();
+                break;
+        }
+    }
 }
