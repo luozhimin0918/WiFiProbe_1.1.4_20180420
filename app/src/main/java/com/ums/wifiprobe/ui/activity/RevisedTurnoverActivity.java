9c07a231afa1cdcdc9c559dd3b022bfe9681ca96
luozhimin <838111691@qq.com>
2018-07-11 16:35:19 +0800

    RevisedTurnoverActivity  编辑 修正交易金额 自动修改 修正比例

--- app/src/main/java/com/ums/wifiprobe/ui/activity/RevisedTurnoverActivity.java
+++ app/src/main/java/com/ums/wifiprobe/ui/activity/RevisedTurnoverActivity.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -80,7 +82,7 @@ public class RevisedTurnoverActivity extends BaseActivity implements View.OnClic
     String XiuzhenNumStr;
     String XiuzhenBulieStr;
     String ZhandiMianji;
-
+    boolean  isEditXiuZhen=false;
     @Override
     public void initData() {
         Intent intent = getIntent();
@@ -95,8 +97,88 @@ public class RevisedTurnoverActivity extends BaseActivity implements View.OnClic
             xiuzhenBulieStr.setText(XiuzhenBulieStr+"%");
             zhandiMianji.setText(ZhandiMianji+"㎡");
         }
-    }
+        editQuery.addTextChangedListener(watcherXiu);
+        editQueryBilie.addTextChangedListener(watcherBili);
 
+       editQuery.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b){
+                    isEditXiuZhen=true;
+                }else{
+                    isEditXiuZhen=false;
+                }
+
+            }
+        });
+
+
+    }
+    TextWatcher watcherXiu = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //只要编辑框内容有变化就会调用该方法，s为编辑框变化后的内容
+            Log.i("onTextChanged", s.toString());
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //编辑框内容变化之前会调用该方法，s为编辑框内容变化之前的内容
+            Log.i("beforeTextChanged", s.toString());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //编辑框内容变化之后会调用该方法，s为编辑框内容变化后的内容
+            Log.i("afterTextChanged", s.toString());
+
+            if(isEditXiuZhen){
+                if(s.toString()!=null&&!s.toString().equals("")){
+                    float  xiuzhen= Float.parseFloat(s.toString());
+                    float  money = Float.parseFloat(MoneyZong);
+                    float  baili = (xiuzhen/money-1)*100;
+                    editQueryBilie.setText(baili+"");
+                }else{
+                    editQueryBilie.setText("");
+                }
+            }
+
+
+        }
+    };
+    TextWatcher watcherBili = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //只要编辑框内容有变化就会调用该方法，s为编辑框变化后的内容
+            Log.i("onTextChanged", s.toString());
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //编辑框内容变化之前会调用该方法，s为编辑框内容变化之前的内容
+            Log.i("beforeTextChanged", s.toString());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //编辑框内容变化之后会调用该方法，s为编辑框内容变化后的内容
+            Log.i("afterTextChanged", s.toString());
+            if(!isEditXiuZhen){
+                if(s.toString()!=null&&!s.toString().equals("")){
+                    float  bi= Float.parseFloat(s.toString());
+                    float  money = Float.parseFloat(MoneyZong);
+                    float  xiuzhenTo = (bi/100+1)*money;
+                    editQuery.setText(xiuzhenTo+"");
+                }else{
+                    editQuery.setText("");
+                }
+            }
+
+
+        }
+    };
     @Override
     public int getLayoutId() {
         return R.layout.activity_revised_turnover;
